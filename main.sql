c0536f54f1420f4e39bfb24cb0019b5e53aab692
bhautik patel <bdudhat293@gmail.com>
2024-01-02 10:04:51 +0530

    02-01-2023

---
+++ main.sql
@@ -0,0 +1,21 @@
+create table user(
+        id int,
+        firstname varchar(20),
+        lastname varchar(30),
+        gender varchar(20),
+        email varchar(30),
+        mobileNo int
+);
+
+insert into user (id,firstname,lastname,gender,email,mobileNo) values(1,'bhautik','Dudhat','Male','bhautik@test.in',46646787);
+
+insert into user (id,firstname,lastname,gender,email,mobileNo)values
+(2,'sachin','tendulkar','male','sachin@test.in',6484),
+(3,'virat','kohli','Male','chuku@test.in',27187),
+(4,'virrendra','sehvag','Male','viru@test.in',1871),
+(5,'rohit','sharma','Male','rohit@test.in',1154);
+
+insert into user values(6,'kl','rahul','Male','kl@test.in',616979);
+
+insert into user (id,firstname,lastname,gender,mobileNo)values
+(7,'jasprit','bumrah','Male',18798718);
---
+++ table.sql
@@ -0,0 +1,18 @@
+create table user2 (
+    CLIENTNO varchar(6),
+    NAME  varchar(20),
+    ADDRESS1 varchar(30),
+    CITY  varchar(15),
+    PINCODE varchar(8),
+    STATE varchar(15),
+    BALDUE int
+);
+
+insert into user2 (CLIENTNO,NAME,ADDRESS1,CITY,PINCODE,STATE,BALDUE) values('C00001','Ivan bayross','A/14','Mumbai','400054','Maharashtra',15000);
+
+insert into user2 (CLIENTNO,NAME,ADDRESS1,CITY,PINCODE,STATE,BALDUE) values
+('C00002','Mamta Muzumbar','65','Madras','780001','Tamil Nadu',0),
+('C00003','chhaya Bansar','P-7','Mumbai','400057','Maharastra',5000),
+('C00004','Ashwini joshi','A/5','Banglore','560001','Karnataka',0),
+('C00005','Hansel Colaco','juhu','Munbai','400060','Maharashtra',2000),
+('C00006','Deepk sharma','Bandra','Mangolore','560050','Karnataka',0);
